43cc82bc59b5274f753110123106c4670da0a188
Myrza6ek <foolbull35@gmail.com>
2022-10-01 17:37:45 +0600

    second commit

--- js/main.js
+++ js/main.js
@@ -3,43 +3,6 @@
 // Задание №1
 // Создать программу - список покемонов.
 
-// Пример:
-// Bulbasaur
-// Ivysaur
-// Venusaur
-// Charmander
-// Charmeleon
-// Charizard
-// Squirtle
-// … и т.п.
-
-// При клике на имя покемона, показать рядом (в соседнем div-е) или во всплывающем окне информацию об этом покемоне, например:
-
-// Имя: Charmeleon
-// Тип: fire
-// Рост: 11
-// Вес: 190
-// Изображение покемона (дополнительно)
-
-// Указания:
-// Список покемонов (первые 20 штук) получить через запрос к API:
-// https://pokeapi.co/api/v2/pokemon/
-// Информацию о каждом покемоне получать через запрос к API:
-// https://pokeapi.co/api/v2/pokemon/{id}/
-// где {id} - номер покемона
-// Подсказка об используемых ключах результата
-// (предположим что полученный объект у вас лежит в переменной result)
-// Изображение: result.sprites.front_default
-// Имя: result.name
-// Тип: массив result.types. Из каждого элемента массива можно взять только type.name
-// Рост: result.height
-// Вес: result.weight
-
-// Дополнительно:
-// Используя ссылку на следующую страницу в результате (ссылку на API следующих результатов) реализовать пагинацию (постраничный вывод) в программе, т.е.:
-// На клик по ссылке “Next” делать запрос на следующие 20 штук, заменять текущий список.
-// Реализовать “Previous” и “Next” - возможность возвращаться на страницу ранее
-
 let mainDiv = document.getElementsByClassName("container")[0];
 
 let ol = document.getElementById("orderList");
@@ -51,96 +14,113 @@ let innerModal = document.getElementById("innerModal");
 // let br = document.createElement("br");
 // ol.append(br);
 let btnNext = document.getElementById("next");
+let btnPrev = document.getElementById("prev");
 
-let pok = fetch("https://pokeapi.co/api/v2/pokemon/");
-pok
+let pokems = fetch("https://pokeapi.co/api/v2/pokemon/");
+
+let somethng = "";
+pokems
   .then(res => {
     return res.json();
   })
   .then(res => {
-    // console.log(res);
+    console.log(res);
 
-    res.results.forEach(elem => {
-      // console.log(elem.url);
+    function pokemsFunc(param) {
+      somethng = param;
+      param.results.forEach(elem => {
+        // console.log(elem.url);
 
-      let a = fetch(elem.url);
-      a.then(resp => {
-        // console.log(resp);
-        return resp.json();
-      }).then(respo => {
-        // console.log(respo.name);
-        let li = document.createElement("li");
-        li.innerText = respo.name;
-        li.style.color = "#e50604";
-        ol.append(li);
-        // console.log(respo);
-        // click
-        li.addEventListener("click", () => {
-          blockModal.style.display = "block";
+        let a = fetch(elem.url);
+        a.then(resp => {
+          // console.log(resp);
+          return resp.json();
+        }).then(respo => {
+          // console.log(respo.name);
+          let li = document.createElement("li");
+          li.innerText = respo.name;
+          li.style.color = "#e50604";
+          ol.append(li);
+          // console.log(respo);
+          // click
+          li.addEventListener("click", () => {
+            blockModal.style.display = "block";
 
-          //create image in modal block
-          let img = document.createElement("img");
-          img.setAttribute("src", respo.sprites.front_default);
-          img.style.width = "180px";
-          img.style.height = "195px";
-          innerModal.append(img);
+            //create image in modal block
+            let img = document.createElement("img");
+            img.setAttribute("src", respo.sprites.front_default);
+            img.style.width = "180px";
+            img.style.height = "195px";
+            innerModal.append(img);
 
-          let divAbout = document.createElement("div");
-          divAbout.style.width = "300px";
-          divAbout.style.height = "150px";
-          divAbout.style.fontSize = "26px";
-          divAbout.style.fontWeight = "bold";
-          divAbout.style.textAlign = "center";
+            let divAbout = document.createElement("div");
+            divAbout.style.width = "300px";
+            divAbout.style.height = "150px";
+            divAbout.style.fontSize = "26px";
+            divAbout.style.fontWeight = "bold";
+            divAbout.style.textAlign = "center";
 
-          divAbout.innerText = `Имя: ${respo.name}
-          Тип: ${respo.types[0].type.name}
-          Рост: ${respo.height}
-          Вес:  ${respo.weight}`;
-          innerModal.append(divAbout);
+            divAbout.innerText = `Имя: ${respo.name}
+            Тип: ${respo.types[0].type.name}
+            Уровень: ${respo.height}
+            Ресурсы:  ${respo.weight}`;
+            innerModal.append(divAbout);
 
-          let closeBtn = document.createElement("button");
-          closeBtn.innerText = "CLOSE";
-          closeBtn.style.borderRadius = "5px";
-          closeBtn.style.backgroundColor = "gold";
-          closeBtn.style.color = "#e50604";
-          closeBtn.style.fontSize = "20px";
+            let closeBtn = document.createElement("button");
+            closeBtn.innerText = "CLOSE";
+            closeBtn.style.borderRadius = "5px";
+            closeBtn.style.backgroundColor = "gold";
+            closeBtn.style.color = "#e50604";
+            closeBtn.style.fontSize = "20px";
 
-          innerModal.prepend(closeBtn);
+            innerModal.prepend(closeBtn);
 
-          closeBtn.addEventListener("click", () => {
-            blockModal.style.display = "none";
-            innerModal.innerHTML = "";
-          });
-        });
-      });
-    });
-
-    btnNext.addEventListener("click", () => {
-      ol.innerText = "";
-      let nxt = fetch(res.next);
-      nxt
-        .then(nxte => {
-          return nxte.json();
-        })
-        .then(next => {
-          // console.log(next);/
-          // console.log(next.results);
-          next.results.forEach(elem => {
-            let b = fetch(elem.url);
-            b.then(resp => {
-              // console.log(resp);
-              return resp.json();
-            }).then(respo => {
-              // console.log(respo.name);
-              let li = document.createElement("li");
-              li.innerText = respo.name;
-              li.style.color = "#e50604";
-              ol.append(li);
+            closeBtn.addEventListener("click", () => {
+              blockModal.style.display = "none";
+              innerModal.innerHTML = "";
             });
           });
         });
+      });
+    }
+
+    pokemsFunc(res);
+
+    btnNext.addEventListener("click", () => {
+      if (!somethng.next) {
+        alert("The last page!!!");
+      } else {
+        let nxt = fetch(somethng.next);
+        nxt
+          .then(nxte => {
+            return nxte.json();
+          })
+          .then(nexto => {
+            console.log(nexto);
+            ol.innerHTML = "";
+            pokemsFunc(nexto);
+          });
+      }
+    });
+
+    btnPrev.addEventListener("click", () => {
+      if (!somethng.previous) {
+        alert("the first page");
+      } else {
+        let prvs = fetch(somethng.previous);
+        prvs
+          .then(prev => {
+            return prev.json();
+          })
+          .then(prevss => {
+            console.log(prevss);
+            ol.innerHTML = "";
+            pokemsFunc(prevss);
+          });
+      }
     });
   });
+
 // Задание №2
 // Создать страницу прогноза погоды.
 
